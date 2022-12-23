26f41abd890a728bb9902d3086a16e3626d686a6
looju <omofade2020@gmail.com>
2022-12-23 01:29:44 +0000

    Implementing fonts

--- App.js
+++ App.js
@@ -6,24 +6,46 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NewsNavigator } from "./src/Navigation/NewsNavigator/NewsNavigator";
 import { Carousel } from "./src/Features/Carousel/Screen/Carousel";
 import { Setup } from "./src/Features/Setup/Screen/Setup";
+import {
+  useFonts,
+  Nunito_400Regular,
+  Lato_400Regular,
+  Arizonia_400Regular,
+  Griffy_400Regular,
+  Tangerine_400Regular,
+} from "@expo-google-fonts/dev";
 
 const Stack = createStackNavigator();
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Carousel"
-          component={Carousel}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Setup"
-          component={Setup}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="NewsNavigator" component={NewsNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+
+  
+  let [fontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Lato_400Regular,
+    Arizonia_400Regular,
+    Griffy_400Regular,
+    Tangerine_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Carousel"
+            component={Carousel}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Setup"
+            component={Setup}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="NewsNavigator" component={NewsNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
