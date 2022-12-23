ab3556741bf45a970462a5bcfc5a295900ecace2
looju <omofade2020@gmail.com>
2022-12-23 03:51:51 +0000

    basic loading screen overview

--- src/Features/Setup/Screen/Setup.js
+++ src/Features/Setup/Screen/Setup.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import {View, Text,StyleSheet} from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-export const Setup = () => {
+export const Setup = ({navigation}) => {
+  const [loading, setLoading] = useState(true);
+
+useEffect(()=>{
+setTimeout(()=>{
+navigation.replace("NewsNavigator")
+})
+},[])
+
+
+setTimeout(changeState, 3000);
+
+
+  function changeState(){
+    setLoading(!loading);
+  };
+
+  const LoadingResource = () => {
     return (
-        <View>
-            <Text>Setup</Text>
-        </View>
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Text>Loading</Text>
+      </View>
     );
-}
+  };
 
-const styles = StyleSheet.create({})
+  return loading ? <LoadingResource /> : null;
+};
 
-
+const styles = StyleSheet.create({});
