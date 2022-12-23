4c7513779140b78b33dbe708a27dc621b9a4b3d7
looju <omofade2020@gmail.com>
2022-12-23 01:24:08 +0000

    Initial setup

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { createStackNavigator} from '@react-navigation/stack';
-import { NewsNavigator } from './src/Navigation/NewsNavigator/NewsNavigator';
-import { Carousel } from './src/Features/Carousel/Screen/Carousel';
-import { Setup } from './src/Features/Setup/Screen/Setup';
-
+import "react-native-gesture-handler";
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NewsNavigator } from "./src/Navigation/NewsNavigator/NewsNavigator";
+import { Carousel } from "./src/Features/Carousel/Screen/Carousel";
+import { Setup } from "./src/Features/Setup/Screen/Setup";
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
- <Stack.Navigator>
-  <Stack.Screen name="Carousel" component={Carousel}/>
-  <Stack.Screen name="Setup" component={Setup}/>
-  <Stack.Screen name="NewsNavigator" component={NewsNavigator}/>
- </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Carousel"
+          component={Carousel}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Setup"
+          component={Setup}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="NewsNavigator" component={NewsNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-
--- src/Navigation/NewsNavigator/NewsNavigator.js
+++ src/Navigation/NewsNavigator/NewsNavigator.js
@@ -1,31 +1,47 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Tab = createMaterialBottomTabNavigator();
 
 export const NewsNavigator = () => {
- const AllScreen=()=>{
-    return(
-        <View>
-            <Text>
-                Home Screen
-            </Text>
-        </View>
-    )
- }
+  const AllScreen = () => {
+    return (
+      <View>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  };
 
- const SportScreen=()=>{
-    return(
-        <View>
-            <Text>
-                Sport Screen
-            </Text>
-        </View>
-    )
- }
+  const SportScreen = () => {
+    return (
+      <View>
+        <Text>Sport Screen</Text>
+      </View>
+    );
+  };
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused
+              ? "ios-information-circle"
+              : "ios-information-circle-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "ios-list" : "ios-list-outline";
+          }
+
+          // You can return any component that you like here!
+          return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen name="Home" component={AllScreen} />
       <Tab.Screen name="Settings" component={SportScreen} />
     </Tab.Navigator>
